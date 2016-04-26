1861118f652f77e2bc1e30de1f9e00764ac05a69
Josue Valverde <josuevalverde12@gmail.com>
2016-04-26 11:31:50 -0700

    add task model with title, text, author(user), date started, and date ended fields.

---
+++ ToDo/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class Task(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    crated_date = models.DateTimeField(
+        default=timezone.now)
+    completed_date = models.DateTimeField(
+        blank=True, null=True)
+
+    def complete(self):
+        self.completed_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
